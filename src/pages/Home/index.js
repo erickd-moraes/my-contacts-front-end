3d4ac987b755864baed44fd4d30820648672b817
Erick Moraes <erickd.moraes@gmail.com>
2022-03-31 18:43:28 -0300

    create Home Page and refactoring Header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,91 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Home';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Seta" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Érick Moraes</strong>
+            <small>instagram</small>
+          </div>
+          <span>erickd.moraes@gmail.com</span>
+          <span>(15) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Editar" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Érick Moraes</strong>
+            <small>instagram</small>
+          </div>
+          <span>erickd.moraes@gmail.com</span>
+          <span>(15) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Editar" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Érick Moraes</strong>
+            <small>instagram</small>
+          </div>
+          <span>erickd.moraes@gmail.com</span>
+          <span>(15) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Editar" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,10 +4,30 @@ export const Container = styled.div`
   margin-top: 32px;
 `;
 
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    height: 50px;
+    border: none;
+    border-radius: 25px;
+    background: #FFF;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    padding: 0 16px;
+    outline: 0;
+
+    &::placeholder {
+      color: #BCBCBC;
+    }
+  }
+`;
+
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;
