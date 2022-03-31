585ce2b4f334d62da0833cf02f15c0a85dd7d63d
Erick Moraes <erickd.moraes@gmail.com>
2022-03-31 17:02:55 -0300

    create the contact list

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222;
     font-size: 24px;
   }
 
@@ -26,6 +25,76 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #FFF;
+    }
+  }
+`;
+
+export const ListContainer = styled.header`
+  margin-top: 24px;
+  margin-bottom: 8px;
+
+  header {
+
+    button {
+      display: flex;
+      align-items: center;
+      background: transparent;
+      border: none;
+
+      span {
+        font-weight: bold;
+        margin-right: 8px;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #FFF;
+  padding: 16px;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        font-weight: bold;
+        text-transform: uppercase;
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        padding: 4px;
+        border-radius: 4px;
+        margin-left: 8px;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray.light};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
