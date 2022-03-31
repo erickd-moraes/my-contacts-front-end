49b63f26b5886bfb3f6113b55783808e8c0c2a56
Erick Moraes <erickd.moraes@gmail.com>
2022-03-31 19:18:36 -0300

    create Input and Select components

---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  font-size: 16px;
+  padding: 0 16px;
+  background: #FFF;
+  border-radius: 4px;
+  border: 2px solid #FFF;
+  outline: none;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  transition: border-color .2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
