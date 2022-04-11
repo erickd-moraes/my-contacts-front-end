343c2b1557ee93b8e6cb4daeb66e70654425bb0c
Erick Moraes <erickd.moraes@gmail.com>
2022-04-11 14:29:31 -0300

    create and add behavior to emptyListContainer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,25 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-nested-ternary */
 import { Link } from 'react-router-dom';
 import {
   useState, useEffect, useMemo, useCallback,
 } from 'react';
 
 import {
-  Container, InputSearchContainer, Header, ListHeader, Card, ErrorContainer,
+  Container,
+  InputSearchContainer,
+  Header,
+  ListHeader,
+  Card,
+  ErrorContainer,
+  EmptyListContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
+import emptyBox from '../../assets/images/empty-box.svg';
 
 import formatPhone from '../../utils/formatPhone';
 
@@ -64,17 +73,29 @@ export default function Home() {
     <Container>
       <Loader isloading={isloading} />
 
-      <InputSearchContainer>
-        <input
-          type="text"
-          placeholder="Pesquise pelo nome..."
-          value={searchTerm}
-          onChange={handleChangeSearchTerm}
-        />
-      </InputSearchContainer>
+      {contacts.length > 0 && (
+        <InputSearchContainer>
+          <input
+            type="text"
+            placeholder="Pesquise pelo nome..."
+            value={searchTerm}
+            onChange={handleChangeSearchTerm}
+          />
+        </InputSearchContainer>
+      )}
 
-      <Header hasError={hasError}>
-        {!hasError && (
+      <Header
+        justifyContent={
+          hasError
+            ? 'flex-end'
+            : (
+              contacts.length > 0
+                ? 'space-between'
+                : 'center'
+            )
+        }
+      >
+        {(!hasError && contacts.length > 0) && (
           <strong>
             {filteredContacts.length}
             {filteredContacts.length === 1 ? ' contato' : ' contatos'}
@@ -99,13 +120,24 @@ export default function Home() {
 
       {!hasError && (
         <>
+          {(contacts.length < 1 && !isloading) && (
+            <EmptyListContainer>
+              <img src={emptyBox} alt="Caixa vazia" />
+              <p>
+                Você ainda não tem nenhum contato cadastrado!
+                Clique no botão <strong>”Novo contato”</strong> à cima para
+                cadastrar o seu primeiro!
+              </p>
+            </EmptyListContainer>
+          )}
+
           {filteredContacts.length > 0 && (
-          <ListHeader orderBy={orderBy}>
-            <button type="button" onClick={handleToogleOrderBy}>
-              <span>Nome</span>
-              <img src={arrow} alt="Seta" />
-            </button>
-          </ListHeader>
+            <ListHeader orderBy={orderBy}>
+              <button type="button" onClick={handleToogleOrderBy}>
+                <span>Nome</span>
+                <img src={arrow} alt="Seta" />
+              </button>
+            </ListHeader>
           )}
 
           {filteredContacts.map((contact) => (
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -27,7 +27,7 @@ export const InputSearchContainer = styled.div`
 export const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: ${({ hasError }) => (hasError ? 'flex-end' : 'space-between')};
+  justify-content: ${({ justifyContent }) => justifyContent};
   margin-top: 32px;
   border-bottom: 2px solid ${({ theme }) => theme.colors.gray.lighter};
   padding-bottom: 16px;
@@ -137,6 +137,22 @@ export const ErrorContainer = styled.div`
       font-size: 22px;
       color: ${({ theme }) => theme.colors.danger.main};
       margin-bottom: 8px;
+    }
+  }
+`;
+export const EmptyListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 16px;
+
+  p {
+    text-align: center;
+    color: ${({ theme }) => theme.colors.gray.light};
+    margin-top: 8px;
+
+    strong {
+      color: ${({ theme }) => theme.colors.primary.main};
     }
   }
 `;
